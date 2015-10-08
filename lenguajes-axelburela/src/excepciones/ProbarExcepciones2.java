3106f6582411aee081d5afca96ba5574fdd68a1c
T-107 <T-107@PC244>
2015-10-08 18:24:32 -0500

    08/10/15

---
+++ lenguajes-axelburela/src/excepciones/ProbarExcepciones2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones;
+
+public class ProbarExcepciones2 {
+    public static void main(String[] args) {
+        //Vamos a crear un usuario
+        Usuario u=null;
+        System.out.println( u.getNombre());
+    }
+}
